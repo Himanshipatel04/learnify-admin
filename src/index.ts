4fdb8281d18f0970286f93dfa9e28999cb163e9f
Himanhipatel04 <himanshipatel0409@gmail.com>
2025-11-08 08:38:24 +0530

    feat: initialize admin API with Express and TypeScript
    - Added package.json to manage dependencies and scripts.
    - Created src/index.ts as the entry point for the application.
    - Configured TypeScript with tsconfig.json for strict type checking and modern JavaScript features.

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import bodyParser, { urlencoded } from "body-parser";
+import express from "express";
+import type { Request, Response } from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({ message: "Admin api is listening." });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Admin app is running at ${process.env.PORT}`);
+});
